9c451d0d9ebbf45d26f3cd14ccc016a40bc5a4d3
Loic <loic@stokr.io>
2026-02-02 08:51:02 +0100

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
 import { Router } from './Router'
 import { headerLinks } from './routes'
 import { Header } from './components/Header'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <Header links={headerLinks} />
       <div className="card">
         <Router />
-        <h1>Vite + React</h1>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
     </div>
   )
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Homepage = () => {
+  const [count, setCount] = useState(0)
   return (
     <div>
       <h1>Homepage</h1>
       <Link to="/about">About</Link>
+      <h1>Vite + React</h1>
+      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
     </div>
   )
 }
